fb9f0977a9a8f695f74ed801be25c87787ba13c5
Ferdinando Simonetti <info@fsimonetti.eu>
2020-04-06 18:55:43 +0200

    fix: paper-start.sh - again

--- bin/paper-start.sh
+++ bin/paper-start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export PAPERVERSION="162"
-export SERVER=$(find $HOME -name paper-${PAPERVERSION})
+echo $PAPERVERSION
+export SERVER="$(find $HOME -name paper-${PAPERVERSION})"
 echo $SERVER
-cd $(dirname $SERVER)
-java -Xmx6144m -Xms6144m -jar $SERVER nogui
+cd $( dirname ${SERVER} )
+java -Xmx6144m -Xms6144m -jar ${SERVER} nogui
 cd -
