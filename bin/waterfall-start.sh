2a568752d1f8df49142baeb01e5a5d74e82ddb3b
Ferdinando Simonetti <info@fsimonetti.eu>
2020-04-07 23:22:17 +0200

    fix: waterfall-start.sh

--- bin/waterfall-start.sh
+++ bin/waterfall-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export WFALLVERSION="327"
 echo $WFALLVERSION
-export SERVER="$(find $HOME -name paper-${WFALLVERSION}.jar)"
+export SERVER="$(find $HOME -name waterfall-${WFALLVERSION}.jar)"
 echo $SERVER
 cd $(dirname $SERVER)
 java -Xmx4096m -Xms4096m -jar ${SERVER} nogui
